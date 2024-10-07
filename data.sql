390d84564375bb2d396339b350a3c252e3f8f28c
Marcus Lull <marcuslull@gmail.com>
2024-10-07 13:35:57 -0700

    Updating DDL to reflect best practice and additional constraints.
    Updating schema.sql to include universal ID columns, Created, updated, and isDeleted columns.
    Updating schema.sql to include attitional CHECK constraints for updated colums and new PK in products_categories table.

--- data.sql
+++ data.sql
@@ -1,8 +1,3 @@
--- Inserting test data for products
-INSERT INTO public.products (sku, product_name, quantity, threshold, description, weight)
-VALUES ('SKU123', 'Product 1', 100, 10, 'This is product 1', 1.25),
-       ('SKU124', 'Product 2', 50, 5, 'This is product 2', 2.50),
-       ('SKU125', 'Product 3', 200, 20, 'This is product 3', 0.75);
 
 -- Inserting test data for categories
 INSERT INTO public.categories (category_name)
@@ -10,67 +5,73 @@ VALUES ('Category 1'),
        ('Category 2'),
        ('Category 3');
 
+
+
+-- Inserting test data for products
+INSERT INTO public.products (sku, product_name, quantity, threshold, description, weight)
+VALUES ('SKU123', 'Product 1', 100, 10, 'This is product 1', 1.25),
+       ('SKU124', 'Product 2', 50, 5, 'This is product 2', 2.50),
+       ('SKU125', 'Product 3', 200, 20, 'This is product 3', 0.75);
+
+
+
 -- Inserting test data for suppliers
 INSERT INTO public.suppliers (supplier_name, website, representative, phone)
 VALUES ('Supplier 1', 'https://supplier1.example.com', 'Rep 1', '123-456-7890'),
        ('Supplier 2', 'https://supplier2.example.com', 'Rep 2', '234-567-8901'),
        ('Supplier 3', 'https://supplier3.example.com', 'Rep 3', '345-678-9012');
 
+
+
 -- Inserting test data for price
 INSERT INTO public.price (price, product_id, supplier_id)
-VALUES (9.99, 'SKU123', 1),
-       (19.99, 'SKU124', 2),
-       (29.99, 'SKU125', 3);
+VALUES (9.99, 1, 1),
+       (19.99, 2, 2),
+       (29.99, 3, 3),
+	   (11.99, 1, 2),
+       (12.99, 1, 3),
+       (18.99, 2, 1),
+       (17.99, 2, 3),
+       (22.99, 3, 1),
+       (21.99, 3, 2),
+       (10.50, 1, 1),
+       (15.75, 2, 2),
+       (20.25, 3, 3);
 
--- More price entries for each product from different suppliers
-INSERT INTO public.price (price, product_id, supplier_id)
-VALUES (11.99, 'SKU123', 2),
-       (12.99, 'SKU123', 3),
-       (18.99, 'SKU124', 1),
-       (17.99, 'SKU124', 3),
-       (22.99, 'SKU125', 1),
-       (21.99, 'SKU125', 2),
-       (10.50, 'SKU123', 1),
-       (15.75, 'SKU124', 2),
-       (20.25, 'SKU125', 3);
+
 
 -- Inserting test data for notes
 INSERT INTO public.notes (note, product_id, category_id, supplier_id)
-VALUES ('Note 1 for Product 1', 'SKU123', 1, 1),
-       ('Note 2 for Product 2', 'SKU124', 2, 2),
-       ('Note 3 for Product 3', 'SKU125', 3, 3);
-
--- Inserting more test data for notes
-INSERT INTO public.notes (note, product_id, category_id, supplier_id)
-VALUES ('Additional Note 1 for Product 1 from Supplier 1', 'SKU123', 1, 1),
-       ('Additional Note 2 for Product 2 from Supplier 2', 'SKU124', 2, 2),
-       ('Additional Note 3 for Product 3 from Supplier 3', 'SKU125', 3, 3),
+VALUES ('Note 1 for Product 1', 1, 1, 1),
+       ('Note 2 for Product 2', 2, 2, 2),
+       ('Note 3 for Product 3', 3, 3, 3),
+	   ('Additional Note 1 for Product 1 from Supplier 1', 1, 1, 1),
+       ('Additional Note 2 for Product 2 from Supplier 2', 2, 2, 2),
+       ('Additional Note 3 for Product 3 from Supplier 3', 3, 3, 3),
        ('General Note 1 for Category 1 from Supplier 1', NULL, 1, 1),
        ('General Note 2 for Category 2 from Supplier 2', NULL, 2, 2),
        ('General Note 3 for Category 3 from Supplier 3', NULL, 3, 3),
-       ('Product Note for SKU123 in Category 2', 'SKU123', 2, NULL),
-       ('Product Note for SKU124 in Category 3', 'SKU124', 3, NULL),
-       ('Product Note for SKU125 in Category 1', 'SKU125', 1, NULL);
+       ('Product Note for SKU123 in Category 2', 1, 2, NULL),
+       ('Product Note for SKU124 in Category 3', 2, 3, NULL),
+       ('Product Note for SKU125 in Category 1', 3, 1, NULL);
+
+
 
 -- Inserting test data for products_categories
-INSERT INTO public.products_categories (products_sku, categories_category_id)
-VALUES ('SKU123', 1),
-       ('SKU124', 2),
-       ('SKU125', 3);
+INSERT INTO public.products_categories (products_product_id, categories_category_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+	   (1, 2),
+       (2, 1);
 
-INSERT INTO public.products_categories (products_sku, categories_category_id)
-VALUES ('SKU123', 2),
-       ('SKU124', 1),
-       ('SKU124', 3);
+
 
 -- Inserting test data for products_suppliers
-INSERT INTO public.products_suppliers (products_sku, suppliers_supplier_id)
-VALUES ('SKU123', 1),
-       ('SKU124', 2),
-       ('SKU125', 3);
-
--- Adding more supplier for each product in products_suppliers table
-INSERT INTO public.products_suppliers (products_sku, suppliers_supplier_id)
-VALUES ('SKU123', 2),
-       ('SKU124', 1),
-       ('SKU125', 2);
+INSERT INTO public.products_suppliers (products_product_id, suppliers_supplier_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+	   (1, 2),
+       (2, 1),
+       (3, 2);
--- schema.sql
+++ schema.sql
@@ -1,29 +1,136 @@
-
--- DROP TABLE IF EXISTS public.products_categories CASCADE;
--- DROP TABLE IF EXISTS public.products_suppliers CASCADE;
--- DROP TABLE IF EXISTS public.products CASCADE;
--- DROP TABLE IF EXISTS public.categories CASCADE;
--- DROP TABLE IF EXISTS public.suppliers CASCADE;
--- DROP TABLE IF EXISTS public.price CASCADE;
--- DROP TABLE IF EXISTS public.notes CASCADE;
+BEGIN;
 
 
-CREATE TABLE IF NOT EXISTS public.products
-(
-    sku character varying(20) NOT NULL,
-    product_name character varying(50) NOT NULL,
-    quantity integer NOT NULL DEFAULT 0,
-    threshold integer CHECK (threshold >= 0),
-    description text,
-    weight numeric(6, 2),
-    PRIMARY KEY ("sku")
-);
+
+DROP TRIGGER IF EXISTS update_categories_updated_at ON categories;
+
+DROP TRIGGER IF EXISTS update_notes_updated_at ON notes;
+
+DROP TRIGGER IF EXISTS update_price_updated_at ON price;
+
+DROP TRIGGER IF EXISTS update_products_updated_at ON products;
+
+DROP TRIGGER IF EXISTS update_suppliers_updated_at ON suppliers;
+
+
+
+DROP FUNCTION IF EXISTS update_updated_at_column();
+
+
+
+ALTER TABLE IF EXISTS public.notes DROP CONSTRAINT IF EXISTS notes_category_id_fkey;
+
+ALTER TABLE IF EXISTS public.notes DROP CONSTRAINT IF EXISTS notes_product_id_fkey;
+
+ALTER TABLE IF EXISTS public.notes DROP CONSTRAINT IF EXISTS notes_supplier_id_fkey;
+
+ALTER TABLE IF EXISTS public.price DROP CONSTRAINT IF EXISTS price_product_id_fkey;
+
+ALTER TABLE IF EXISTS public.price DROP CONSTRAINT IF EXISTS price_supplier_id_fkey;
+
+ALTER TABLE IF EXISTS public.products_categories DROP CONSTRAINT IF EXISTS products_categories_categories_category_id_fkey;
+
+ALTER TABLE IF EXISTS public.products_categories DROP CONSTRAINT IF EXISTS products_categories_products_product_id_fkey;
+
+ALTER TABLE IF EXISTS public.products_suppliers DROP CONSTRAINT IF EXISTS products_suppliers_products_product_id_fkey;
+
+ALTER TABLE IF EXISTS public.products_suppliers DROP CONSTRAINT IF EXISTS products_suppliers_suppliers_supplier_id_fkey;
+
+
+
+DROP TABLE IF EXISTS public.products_categories;
+
+DROP TABLE IF EXISTS public.products_suppliers;
+
+DROP TABLE IF EXISTS public.price;
+
+DROP TABLE IF EXISTS public.notes;
+
+DROP TABLE IF EXISTS public.suppliers;
+
+DROP TABLE IF EXISTS public.categories;
+
+DROP TABLE IF EXISTS public.products;
+
+
+
+-- DROP SEQUENCE IF EXISTS categories_category_id_seq;
+
+-- DROP SEQUENCE IF EXISTS notes_note_id_seq;
+
+-- DROP SEQUENCE IF EXISTS price_price_id_seq;
+
+-- DROP SEQUENCE IF EXISTS products_product_id_seq;
+
+-- DROP SEQUENCE IF EXISTS suppliers_supplier_id_seq;
+
+
 
 CREATE TABLE IF NOT EXISTS public.categories
 (
     category_id serial NOT NULL,
     category_name character varying(50) NOT NULL,
-    PRIMARY KEY (category_id)
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT categories_pkey PRIMARY KEY (category_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.notes
+(
+    note_id serial NOT NULL,
+    note text NOT NULL,
+    category_id integer,
+    supplier_id integer,
+    product_id integer,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT notes_pkey PRIMARY KEY (note_id),
+	CONSTRAINT notes_at_least_one_id_required CHECK (category_id IS NOT NULL OR supplier_id IS NOT NULL OR product_id IS NOT NULL)
+);
+
+CREATE TABLE IF NOT EXISTS public.price
+(
+    price_id serial NOT NULL,
+    price numeric(7, 2) NOT NULL,
+    supplier_id integer NOT NULL,
+    product_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT price_pkey PRIMARY KEY (price_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.products
+(
+    product_id serial NOT NULL,
+	sku character varying(20) NOT NULL,
+    product_name character varying(50) NOT NULL,
+    quantity integer NOT NULL DEFAULT 0,
+    threshold integer,
+    description text,
+    weight numeric(6, 2),    
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT products_pkey PRIMARY KEY (product_id),
+    CONSTRAINT products_sku_key UNIQUE (sku),
+	CONSTRAINT products_positive_threshold CHECK (threshold > 0)
+);
+
+CREATE TABLE IF NOT EXISTS public.products_categories
+(
+    categories_category_id integer NOT NULL,
+    products_product_id integer NOT NULL,
+	CONSTRAINT products_categories_pkey PRIMARY KEY (categories_category_id, products_product_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.products_suppliers
+(
+    suppliers_supplier_id integer NOT NULL,
+    products_product_id integer NOT NULL,
+	CONSTRAINT products_suppliers_pkey PRIMARY KEY (suppliers_supplier_id, products_product_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.suppliers
@@ -33,98 +140,120 @@ CREATE TABLE IF NOT EXISTS public.suppliers
     website character varying(200) NOT NULL,
     representative character varying(50),
     phone character varying(15) NOT NULL,
-    PRIMARY KEY (supplier_id)
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT suppliers_pkey PRIMARY KEY (supplier_id)
 );
 
-CREATE TABLE IF NOT EXISTS public.price
-(
-    price_id serial NOT NULL,
-    price numeric(7, 2) NOT NULL,
-    product_id character varying(20) NOT NULL,
-    supplier_id integer NOT NULL,
-    PRIMARY KEY (price_id)
-);
-
-CREATE TABLE IF NOT EXISTS public.notes
-(
-    note_id serial NOT NULL,
-    note text NOT NULL,
-    product_id character varying(20),
-    category_id integer,
-    supplier_id integer,
-    PRIMARY KEY (note_id)
-);
-
-CREATE TABLE IF NOT EXISTS public.products_categories
-(
-    products_sku character varying(20) NOT NULL,
-    categories_category_id serial NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS public.products_suppliers
-(
-    products_sku character varying(20) NOT NULL,
-    suppliers_supplier_id serial NOT NULL
-);
-
-ALTER TABLE IF EXISTS public.price
-    ADD FOREIGN KEY (product_id)
-    REFERENCES public.products (sku) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE;
-
-
-ALTER TABLE IF EXISTS public.price
-    ADD FOREIGN KEY (supplier_id)
-    REFERENCES public.suppliers (supplier_id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE;
 
 
 ALTER TABLE IF EXISTS public.notes
-    ADD FOREIGN KEY (product_id)
-    REFERENCES public.products (sku) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE;
-
-
-ALTER TABLE IF EXISTS public.notes
-    ADD FOREIGN KEY (category_id)
+    ADD CONSTRAINT notes_category_id_fkey FOREIGN KEY (category_id)
     REFERENCES public.categories (category_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
 
 
 ALTER TABLE IF EXISTS public.notes
-    ADD FOREIGN KEY (supplier_id)
+    ADD CONSTRAINT notes_product_id_fkey FOREIGN KEY (product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE;
+
+
+ALTER TABLE IF EXISTS public.notes
+    ADD CONSTRAINT notes_supplier_id_fkey FOREIGN KEY (supplier_id)
+    REFERENCES public.suppliers (supplier_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE;
+
+
+ALTER TABLE IF EXISTS public.price
+    ADD CONSTRAINT price_product_id_fkey FOREIGN KEY (product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE;
+
+
+ALTER TABLE IF EXISTS public.price
+    ADD CONSTRAINT price_supplier_id_fkey FOREIGN KEY (supplier_id)
     REFERENCES public.suppliers (supplier_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
 
 
 ALTER TABLE IF EXISTS public.products_categories
-    ADD FOREIGN KEY (products_sku)
-    REFERENCES public.products (sku) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE;
-
-
-ALTER TABLE IF EXISTS public.products_categories
-    ADD FOREIGN KEY (categories_category_id)
+    ADD CONSTRAINT products_categories_categories_category_id_fkey FOREIGN KEY (categories_category_id)
     REFERENCES public.categories (category_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
 
 
-ALTER TABLE IF EXISTS public.products_suppliers
-    ADD FOREIGN KEY (products_sku)
-    REFERENCES public.products (sku) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.products_categories
+    ADD CONSTRAINT products_categories_products_product_id_fkey FOREIGN KEY (products_product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
 
 
 ALTER TABLE IF EXISTS public.products_suppliers
-    ADD FOREIGN KEY (suppliers_supplier_id)
+    ADD CONSTRAINT products_suppliers_products_product_id_fkey FOREIGN KEY (products_product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE;
+
+
+ALTER TABLE IF EXISTS public.products_suppliers
+    ADD CONSTRAINT products_suppliers_suppliers_supplier_id_fkey FOREIGN KEY (suppliers_supplier_id)
     REFERENCES public.suppliers (supplier_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
+
+
+
+CREATE OR REPLACE FUNCTION update_updated_at_column() 
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW; 
+END;
+$$ language 'plpgsql';
+
+
+
+CREATE TRIGGER update_categories_updated_at
+BEFORE UPDATE ON categories
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TRIGGER update_notes_updated_at
+BEFORE UPDATE ON notes
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TRIGGER update_price_updated_at
+BEFORE UPDATE ON price
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TRIGGER update_products_updated_at
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TRIGGER update_suppliers_updated_at
+BEFORE UPDATE ON suppliers
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+
+
+ALTER SEQUENCE categories_category_id_seq RESTART WITH 1;
+ALTER SEQUENCE notes_note_id_seq RESTART WITH 1;
+ALTER SEQUENCE price_price_id_seq RESTART WITH 1;
+ALTER SEQUENCE products_product_id_seq RESTART WITH 1;
+ALTER SEQUENCE suppliers_supplier_id_seq RESTART WITH 1;
+
+
+END;
