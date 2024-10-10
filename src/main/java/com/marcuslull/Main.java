fc985de904aa652ede31db37c9968eb8823d5445
Marcus Lull <marcuslull@gmail.com>
2024-10-10 16:17:01 -0700

    Adding some simple find requests to use for testing

--- src/main/java/com/marcuslull/Main.java
+++ src/main/java/com/marcuslull/Main.java
@@ -1,5 +1,6 @@
 package com.marcuslull;
 
+import com.marcuslull.entities.*;
 import com.marcuslull.persistence.CustomPersistenceUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -16,8 +17,24 @@ public class Main {
         try {
             em.getTransaction().begin();
 
+            Product product = em.find(Product.class, 1);
+            System.out.println("Product from product: " + product);
+            product.getCategories().forEach(category -> System.out.println("Category from product: " + category));
+            product.getSuppliers().forEach(supplier -> System.out.println("Supplier from product: " + supplier));
+            product.getPrices().forEach(price -> System.out.println("Price from product: " + price));
+            product.getNotes().forEach(note -> System.out.println("Note from product: " + note));
 
+            Category category = em.find(Category.class, 1);
+            System.out.println("Category from category: " + category);
 
+            Supplier supplier = em.find(Supplier.class, 1);
+            System.out.println("Supplier from supplier: " + supplier);
+
+            Price price = em.find(Price.class, 1);
+            System.out.println("Price from price: " + price);
+
+            Note note = em.find(Note.class, 1);
+            System.out.println("Note from note: " + note);
 
             em.getTransaction().commit();
         }
